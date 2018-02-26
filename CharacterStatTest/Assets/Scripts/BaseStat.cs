0e814bed533daeb8dbb1bd82a109c88921ff5cb2
LondonSklart <karlfyrkant@gmail.com>
2018-02-27 00:36:20 +0100

    Ayy

---
+++ CharacterStatTest/Assets/Scripts/BaseStat.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseStat
+{
+    public List<StatBonus> BaseAdditives { get; set; }
+    public int BaseValue { get; set; }
+    public string StatName { get; set; }
+    public string StatDescription { get; set; }
+    public int FinalValue { get; set; }
+
+
+    public BaseStat(int baseValue, string statName, string statDescription)
+    {
+        BaseAdditives = new List<StatBonus>();
+        BaseValue = baseValue;
+        StatName = statName;
+        StatDescription = statDescription;
+    }
+
+
+
+    public void AddBonus(StatBonus statBonus)
+    {
+
+        BaseAdditives.Add(statBonus);
+
+    }
+
+    public void RemoveBonus(StatBonus statBonus)
+    {
+
+        BaseAdditives.Remove(statBonus);
+
+    }
+
+   public int GetCalculatedValue()
+    {
+        BaseAdditives.ForEach(x => FinalValue += x.BonusValue);
+        FinalValue += BaseValue;
+        return FinalValue;
+    }
+
+}
